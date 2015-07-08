7e2a1af0878ace3a1475a2b14f950504dfe26e1a
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-07-08 15:39:11 -0400

    adds authentication

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 var port        = process.env.PORT || 8080;
 var User        = require('./app/models/user');
+var jwt         = require('jsonwebtoken');
+var superSecret = 'sandiegosuperchargers';
 
 
 // db setup
@@ -35,11 +37,77 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// route for authenticating users
+apiRouter.post('/authenticate', function (req, res) {
+//    find the user, select the name username and password explicitly
+    User.findOne({
+        username: req.body.username
+    }).select('name username password').exec(function (err, user) {
+        if (err) throw err;
+
+        //    no user with that username was found
+        if (!user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        } else if (user) {
+            // check if password matches
+            var validPassword = user.comparePassword(req.body.password);
+            if (!validPassword) {
+                res.json({
+                    success: false,
+                    message: 'Authentication failed. Wrong password.'
+                });
+            } else {
+                //    if user is found and password is right
+                //    create token
+                var token = jwt.sign({
+                    name: user.name,
+                    username: user.username
+                }, superSecret, {
+                    expiresInMinutes: 1440 // expires in 24 hrs
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
 // middleware to use for all requests
 apiRouter.use(function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, superSecret, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        })
+    } else {
+        // if there is no token, return an HTTP response of 403 (access forbidden) and an error message
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
     //logging
-    console.log('Somebody just came to our app');
-    next();
+    //console.log('Somebody just came to our app');
+    //next();
 });
 
 // Routes
@@ -113,6 +181,10 @@ apiRouter.route('/users/:user_id')
             res.json({ message: 'Succesfully deleted' });
         });
     });
+
+apiRouter.get('/me', function (req, res) {
+    res.send(req.decoded);
+})
 
 
 app.use('/api', apiRouter);
